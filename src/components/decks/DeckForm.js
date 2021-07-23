5da7884fa12ba28886ea3b7c267f986fca270bb8
Luz Angelique Madrazo <luzm321@yahoo.com>
2021-07-23 14:27:40 -0500

    created deck list, card, detail, and form components to render on profile page

---
+++ src/components/decks/DeckForm.js
@@ -0,0 +1,113 @@
+import React, { useContext, useEffect, useState } from "react";
+import { useHistory, useParams } from 'react-router-dom';
+import { DeckContext } from "./DeckProvider";
+
+
+export const DeckForm = () => {
+
+    const { addDeck, getDecks, getDeckById, updateDeck } = useContext(DeckContext);
+
+    // Define the intial state of the form inputs with useState()
+    const [deck, setDeck] = useState({
+        userId: 0,
+        topic: "",
+        description: "",
+    });
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    const { deckId } = useParams();
+    const history = useHistory();
+
+    useEffect(() => {
+        getDecks()
+        .then(() => {
+          if (deckId){
+            getDeckById(deckId)
+            .then(deck => {
+                setDeck(deck)
+                setIsLoading(false)
+            })
+          } else {
+            setIsLoading(false)
+          }
+        })
+    }, [])
+
+    //when a field changes, update state. The return will re-render and display based on the values in state
+    //Controlled component
+    const handleControlledInputChange = (event) => {
+        /* When changing a state object or array,
+        always create a copy, make changes, and then set state.*/
+        const newDeck = { ...deck }
+        /* Deck is an object with properties.
+        Set the property to the new value
+        using object bracket notation. */
+        newDeck[event.target.id] = event.target.value
+        // update state
+        setDeck(newDeck)
+    };
+
+    const saveNewDeck = () => {
+        const newDeck = {
+          topic: deck.topic,
+          description: deck.description,
+          userId: parseInt(deck.userId),
+        }
+        addDeck(newDeck)
+          .then(() => history.push("/decks"))
+    };
+
+    const saveEditDeck = () => {
+        updateDeck({
+          id: deck.id,
+          topic: deck.topic,
+          description: deck.description,
+          userId: parseInt(deck.userId),
+        })
+        .then(() => history.push(`/decks/detail/${deck.id}`))
+    };
+
+    
+    const handleClickSaveDeck = (event) => {
+        event.preventDefault() //Prevents the browser from submitting the form
+
+
+        if (topic === "" || description === "") {
+        window.alert("Please provide values for both input fields. 👇")
+        } else {
+        //Invoke addDeck passing the new deck object as an argument
+        //Once complete, change the url and display the deck list
+            setIsLoading(true);
+
+            if (deckId) {
+                //PUT - update
+                saveEditDeck()
+            } else {
+                saveNewDeck()
+            }
+        };
+    };
+
+    
+    return (
+        <form className="deckForm">
+          <h2 className="deckForm__title">{deckId ? "Edit Deck" : "New Deck" }</h2>
+          <fieldset>
+            <div className="form-group">
+              <label htmlFor="topic">Deck Topic:</label>
+              <input type="text" id="topic" required autoFocus className="form-control" placeholder="Deck topic..." value={deck.topic} onChange={handleControlledInputChange} />
+            </div>
+          </fieldset>
+          <fieldset>
+            <div className="form-group">
+              <label htmlFor="description">Description:</label>
+              <input type="text" id="description" required autoFocus className="form-control" placeholder="Deck description..." value={deck.description} onChange={handleControlledInputChange} />
+            </div>
+          </fieldset>
+          <button className="handleDeck__btn" disabled={isLoading} onClick={handleClickSaveDeck}>
+          {animalId ? "Save Deck" : "Add Deck" }
+          </button>
+        </form>
+    );
+};
---
+++ src/components/decks/DeckList.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useContext, useState } from "react";
+import { useHistory } from 'react-router-dom';
+import { DeckContext } from "./DeckProvider";
+import { DeckCard } from "./DeckCard";
+import "./Deck.css";
+
+
+export const DeckList = () => {
+
+    const { decks, getDecks } = useContext(DeckContext);
+    const history = useHistory();
+
+    // Empty dependency array - useEffect only runs after first render
+    useEffect(() => {
+        getDecks()
+    }, []);
+
+
+    return (
+        <>
+          <h2 className="deckHeader">My Decks</h2>
+                <button className="addDeckBut" onClick={() => {history.push("/decks/create")}}>
+                Add New Employee
+            </button>
+    
+          <div className="decks">
+            {
+              decks.map(deck => {
+                return <DeckCard key={deck.id} deck={deck} />
+              })
+            }
+          </div>
+        </>
+    )
+};
