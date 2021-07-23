37fedac3cb0401eabd5ae96fc0777b235cf967d8
Luz Angelique Madrazo <luzm321@yahoo.com>
2021-07-23 13:14:28 -0500

    created DeckProvider component containing all fetch methods need for the profile page

---
+++ src/components/decks/DeckProvider.js
@@ -0,0 +1,57 @@
+import React, { useState, createContext } from "react";
+
+
+export const DeckContext = createContext();
+
+export const DeckProvider = (props) => {
+
+    const [decks, setDecks] = useState([]);
+
+    const getDecks = () => {
+        return fetch("http://localhost:8088/decks?_expand=user")
+        .then(res => res.json())
+        .then(setDecks)
+    };
+
+    const addDeck = (deckObj) => {
+        return fetch("http://localhost:8088/decks", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(deckObj)
+        })
+        .then(getDecks)
+    };
+
+    const getDeckById = (id) => {
+        return fetch(`http://localhost:8088/decks/${id}?_expand=user`)
+            .then(res => res.json())
+    };
+
+    const deleteDeck = (deckId) => {
+        return fetch(`http://localhost:8088/decks/${deckId}`, {
+            method: "DELETE"
+        })
+            .then(getDecks)
+    };  
+
+    const updateDeck = (deck) => {
+        return fetch(`http://localhost:8088/decks/${deck.id}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(deck)
+        })
+          .then(getDecks)
+    };
+
+    return (
+        <DeckContext.Provider value={{
+            decks, getDecks, addDeck, deleteDeck, getDeckById, updateDeck
+        }}>
+            {props.children}
+        </DeckContext.Provider>
+    );
+};
