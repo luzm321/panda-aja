47553058e740bfdb894416b5b3dfa737a000b4f0
Luz Angelique Madrazo <luzm321@yahoo.com>
2021-07-28 21:28:14 -0500

    added edit functionality for both front side and back side components of each flashcard

--- src/components/flashCards/TranslationProvider.js
+++ src/components/flashCards/TranslationProvider.js
@@ -1,8 +1,8 @@
 
-export const translatePhrase = (language, phrase) => {
+export const translatePhrase = (toLanguage, phrase, fromLanguage = "en") => {
     // with the parameter values passed to this function such as the language and phrase that store the data to send to the proxy server,
     // create a query which contains those values
-    const query = `&language=${language}&phrase=${phrase}`;
+    const query = `&language=${toLanguage}&phrase=${phrase}&fromLanguage=${fromLanguage}`;
     // fetch to the port where the proxy is being hosted (on terminal type: node server.js -p 3000 -w)
     // but you also need to add the passcode and it has to match with the passcode in the proxy here which is: /api/translate
     // after the passcode a "?" is added which tells the proxy that whatever comes after is a query which the proxy can access the values of
@@ -24,6 +24,16 @@ export const transliteratePhrase = (language, phrase, transliterationCode) => {
     });
 };
 
+
+export const detectLanguage = (phrase) => {
+    const query = `&phrase=${phrase}`;
+    return fetch(`http://localhost:3000/api/detect?${query}`)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log('detection data', data);
+        return data[0].language
+    });
+}
 
 
 // import React, { useState, createContext } from "react";
